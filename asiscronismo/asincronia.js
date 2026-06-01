06f8306ecf08236365844608cb010f00cbb70b04
DIANA TRUJILLO <dianaisatrujillo17@gmail.com>
2026-05-31 21:46:00 -0500

    Add files via upload

---
+++ asiscronismo/asincronia.js
@@ -0,0 +1,14 @@
+console.log(1)
+setTimeout(() => console.log("Hola Mundo"), 1000)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+
+for (let index = 1; index < 999; index++) {
+    console.log("Ya termine este bucle")
+}
